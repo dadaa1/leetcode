3ec346570ba05782c6f2138c81d5a03a9598b5f1
郭大山 <gds@choicesoft.com.cn>
2019-07-01 16:35:49 +0800

    add one problem

---
+++ test/array.js
@@ -0,0 +1,20 @@
+function a() {
+  const arr = [];
+  const len = 5;
+  function random() {
+    return Math.floor(Math.random() * 30) + 2;
+  }
+  function b(arr) {
+    if (arr.length === len) {
+      return arr;
+    }
+    let num = random();
+    if (!arr.includes(num)) {
+      arr.push(num);
+    }
+    return b(arr);
+  }
+  return b(arr);
+}
+
+console.log(a());
