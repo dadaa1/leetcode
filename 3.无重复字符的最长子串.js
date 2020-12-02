bddd16e6ce7da5bd347555e7497e2e55b498c6cd
郭大山 <wb-gds644727@koubei.com>
2020-12-02 13:01:34 +0800

    add some file

--- 3.无重复字符的最长子串.js
+++ 3.无重复字符的最长子串.js
@@ -7,13 +7,33 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLongestSubstring = function(s) {
-  if (s.length < 2) {
+var lengthOfLongestSubstring = function (s) {
+  const hasEq = (str) => {
+    const map = {};
+    for (let v of str) {
+      if (map[v]) {
+        return true;
+      }
+      map[v] = 1;
+    }
+    return false;
+  }
+  const len = s.length;
+  if (len < 2) {
     return 1;
   }
   let start = 0;
-  let end = 2;
-  for (end; end < s.length; end++) {
-    for (start; start < end; start++) {}
+  let end = 1;
+  let max = 1;
+  for (let i = 1; i < len; i++) {
+    const newStr = str.split('').slice(start, end).join('')
+    if (hasEq(newStr)) {
+      start += 1;
+      end += 1;
+    } else {
+      max += 1;
+      end += 1;
+    }
   }
+  return max;
 };
--- 88.合并两个有序数组.js
+++ 88.合并两个有序数组.js
@@ -10,25 +10,53 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var merge = function(nums1, m, nums2, n) {
-  let l = 0;
-  let r = 0;
-  while (l < m && r < n) {
-    if (nums1[l] > nums2[r]) {
-      nums1.push(nums2[r]);
-      r += 1;
+// var merge = function(nums1, m, nums2, n) {
+//   let l = 0;
+//   let r = 0;
+//   while (l < m && r < n) {
+//     if (nums1[l] > nums2[r]) {
+//       nums1.push(nums2[r]);
+//       r += 1;
+//     } else {
+//       nums1.push(nums1[l]);
+//       l += 1;
+//     }
+//   }
+//   while (l < m) {
+//     nums1.push(nums1[l]);
+//     l++;
+//   }
+//   while (r < n) {
+//     nums1.push(nums2[r]);
+//     r++;
+//   }
+//   nums1.splice(0, m + n);
+// };
+
+function merge(nums1, m, nums2, n) {
+  let mm = m;
+  let nn = n;
+  const list = [];
+  while (nn || mm) {
+    if (nn === 0) {
+      list.unshift(nums1[mm - 1]);
+      mm -= 1;
+      continue;
+    }
+    if (mm === 0) {
+      list.unshift(nums2[nn - 1]);
+      nn -= 1;
+      continue;
+    }
+    if (nums1[mm - 1] >= nums2[nn - 1]) {
+      list.unshift(nums1[mm - 1]);
+      mm -= 1;
     } else {
-      nums1.push(nums1[l]);
-      l += 1;
+      list.unshift(nums2[nn - 1]);
+      nn -= 1;
     }
   }
-  while (l < m) {
-    nums1.push(nums1[l]);
-    l++;
+  for (let i = 0; i < m + n; i++) {
+    nums1[i] = list[i]
   }
-  while (r < n) {
-    nums1.push(nums2[r]);
-    r++;
-  }
-  nums1.splice(0, m + n);
-};
+}
